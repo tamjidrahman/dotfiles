92344e4f62d517b0644fb5bcd5886d6cd81c3788
Tamjid Rahman <tamjidrahman@tamjids-mbp.lan>
2024-06-25 21:19:16 -0400

    add page down and up with recentering

--- nvim/lua/tamjidrahman/core/keymaps.lua
+++ nvim/lua/tamjidrahman/core/keymaps.lua
@@ -2,7 +2,8 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with (jk)" } )
+keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down half page also recenters" })
+keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up half page also recenters" })
 
 keymap.set("n", "<leader>h", ":nohl<CR>", { desc = "Clear search highlights" })
 
@@ -14,13 +15,10 @@ keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make windows equal size" })
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split"})
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
-
-
-
