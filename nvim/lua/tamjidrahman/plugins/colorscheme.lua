af292f9001f0dccc4e4b3ceeb80c6f2eb9813ab3
Tamjid Rahman <tamjidrahman@tamjids-mbp.lan>
2024-06-30 20:49:02 -0400

    change theme to gruvbo-material

--- nvim/lua/tamjidrahman/plugins/colorscheme.lua
+++ nvim/lua/tamjidrahman/plugins/colorscheme.lua
@@ -1,11 +1,7 @@
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000, -- ensure this runs first
-  config = function ()
-    vim.cmd("colorscheme tokyonight")
-
-    require("tokyonight").setup({
-      style = "night",
-    })
-  end
+	"sainnhe/gruvbox-material",
+	priority = 1000, -- ensure this runs first
+	config = function()
+		vim.cmd("colorscheme gruvbox-material")
+	end,
 }
