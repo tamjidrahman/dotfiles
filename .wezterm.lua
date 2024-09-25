c14d0a4433abb45cb2abcad7dab952dc83735e15
Tamjid Rahman <tamjidrahman@Tamjids-MacBook-Pro.local>
2024-09-25 10:45:33 -0400

    update wezterm opacity

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,9 +5,9 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 12
 
+config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
-
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.95
 config.macos_window_background_blur = 10
 
 config.color_scheme = "Gruvbox Dark (Gogh)"
