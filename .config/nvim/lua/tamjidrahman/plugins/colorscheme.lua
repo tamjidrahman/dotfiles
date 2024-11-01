bfa96d4613c7fdf15aefff5aac01fb241b289fd7
Tamjid Rahman <tamjidarrahman@gmail.com>
2024-11-01 15:13:12 -0400

    set transparent background for nvim

--- .config/nvim/lua/tamjidrahman/plugins/colorscheme.lua
+++ .config/nvim/lua/tamjidrahman/plugins/colorscheme.lua
@@ -3,5 +3,18 @@ return {
 	priority = 1000, -- ensure this runs first
 	config = function()
 		vim.cmd("colorscheme gruvbox-material")
+
+		-- set transparent background
+		vim.cmd([[
+      highlight Normal guibg=none
+      highlight NormalNC guibg=none
+      highlight NvimTreeNormal guibg=none
+      highlight NvimTreeNormalNC guibg=none
+      highlight NonText guibg=none
+      highlight Normal ctermbg=none
+      highlight NormalNC ctermbg=none
+      highlight NonText ctermbg=none
+      highlight NvimTreeEndOfBuffer guibg=none
+    ]])
 	end,
 }
