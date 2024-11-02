f33b5579fa47bdddee4664d9ee8aa2105918c74e
Tamjid Rahman <tamjidarrahman@gmail.com>
2024-11-02 17:08:04 -0400

    increase bg blur

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,7 +8,7 @@ config.font_size = 12
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
 config.window_background_opacity = 0.95
-config.macos_window_background_blur = 10
+config.macos_window_background_blur = 40
 
 config.color_scheme = "Gruvbox Dark (Gogh)"
 
