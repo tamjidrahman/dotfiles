9a7ece64720cea80bf26ae3bcae3bc100090046d
Tamjid Rahman <tamjidarrahman@gmail.com>
2025-02-22 10:48:32 -0500

    update lazyvim

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,51 @@
+return {
+  "sainnhe/gruvbox-material",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- vim.cmd([[
+    --   highlight Normal guibg=none
+    --   highlight NormalNC guibg=none
+    --   highlight NvimTreeNormal guibg=none
+    --   highlight NvimTreeNormalNC guibg=none
+    --   highlight NonText guibg=none
+    --   highlight Normal ctermbg=none
+    --   highlight NormalNC ctermbg=none
+    --   highlight NonText ctermbg=none
+    --   highlight NvimTreeEndOfBuffer guibg=none
+    -- ]])
+    -- Optionally configure and load the colorscheme
+    -- directly inside the plugin declaration.
+    vim.g.gruvbox_material_enable_italic = true
+    vim.cmd.colorscheme("gruvbox-material")
+
+    -- set transparent background
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { ctermbg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { ctermbg = "none" })
+    vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NonText", { ctermbg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Terminal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Folded", { bg = "none" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+
+    -- transparent background for neotree
+    vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NeoTreeVertSplit", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "none" })
+
+    -- transparent background for nvim-tree
+    vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NvimTreeNormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NvimTreeVertSplit", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NvimTreeEndOfBuffer", { bg = "none" })
+  end,
+}
